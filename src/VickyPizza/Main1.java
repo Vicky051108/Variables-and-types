6b10406f0c9fee5ce9c98c4eea6c392ed05499da
Your nam e <Your>
2025-03-02 22:31:04 +0800

    VickyPizza

---
+++ src/VickyPizza/Main1.java
@@ -0,0 +1,16 @@
+package VickyPizza;
+
+public class Main1 {
+    public static void main(String[] args) {
+             VickyPizza pizzaShop = new VickyPizza("Vicky's Pizza", "123 Main St", "info@vickypizza.com", "123-456-7890", "Margherita Pizza", "Tomato, Cheese, Basil", 10.99, "Garlic Bread", "Coke", 1, 15.99, "12345678901234", "12/25", 123);
+            pizzaShop.takeOrder();
+            pizzaShop.makePizza();
+        }
+    }
+        
+           
+            
+    
+    
+
+
