d50000c157a3147261e30fe5427c2435fe1172f4
Your nam e <Your>
2025-03-06 14:00:26 +0800

    111

---
+++ src/VickyPizzaforEx6/Main.java
@@ -0,0 +1,7 @@
+package VickyPizzaforEx6;
+public class Main {
+    public static void main(String[] args) {
+        VickyPizza slice = new VickyPizza();
+        slice.takeOrder();
+    }
+}        
